abf3c8668240eea3a5239ec4a1e4aa6b5081247c
Mike Day <mike.day@rackspace.com>
2015-09-22 14:57:28 -0400

    clean up warning

--- libsandbox.c
+++ libsandbox.c
@@ -15,7 +15,7 @@ void make_sandbox_writeable(void *start, void *end)
 	
 	if (mprotect(start, end - start, PROT_READ|PROT_EXEC|PROT_WRITE))
 	{
-		DMSG("memprotect failed, %s\n", errno);
+		DMSG("memprotect failed, %i\n", errno);
 		return;
 		
 	}
