f2ae9cb42b49d5b1f2831843cdb52e3941165337
Mike Day <mike.day@rackspace.com>
2015-10-09 12:16:15 -0400

    use stevens read and write wrappers, add EAGIN to checks.

--- sandbox-listen.c
+++ sandbox-listen.c
@@ -188,5 +188,54 @@ errout:
 	free(name);
 	errno = err;
 	return ccode;
-		
 }
+
+// WRS, with check for Linux EAGAIN
+ssize_t	readn(int fd, void *vptr, size_t n)
+{
+	size_t  nleft;
+	ssize_t nread;
+	char   *ptr;
+	ptr = vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nread = read(fd, ptr, nleft)) < 0) {
+			if (errno == EINTR || errno == EAGAIN)
+				nread = 0;      /* and call read() again */
+			else
+				return (-1);
+		} else if (nread == 0)
+			break;              /* EOF */
+		
+		nleft -= nread;
+		ptr += nread;
+	}
+	return (n - nleft);         /* return >= 0 */
+}
+
+
+
+// WRS, with check for Linux EAGAIN
+ssize_t writen(int fd, const void *vptr, size_t n)
+{
+	size_t nleft;
+	ssize_t nwritten;
+	const char *ptr;
+	
+	ptr = vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ( (nwritten = write(fd, ptr, nleft)) <= 0) {
+			if (nwritten < 0 && (errno == EINTR || errno == EAGAIN))
+				nwritten = 0;   /* and call write() again */
+			else
+				return (-1);    /* error */
+		}
+		
+		nleft -= nwritten;
+		ptr += nwritten;
+	}
+	return (n);
+}
+
+
