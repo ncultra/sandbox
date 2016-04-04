90418bc58a3d487923ed59cc36b56f750c718513
Mike Day <ncmike@ncultra.org>
2016-04-04 09:40:03 -0400

    remove cli_connect function

--- sandbox-listen.c
+++ sandbox-listen.c
@@ -352,63 +352,6 @@ errout:
 	return SANDBOX_ERR_BAD_FD;
 }
 
-
-#define CLI_PERM S_IRWXU
-int cli_conn(const char *sockpid) 
-{
-	int fd, len, err, rval;
-	struct sockaddr_un un, sun;
-	int do_unlink = 0;
-	if (strlen(sockpid) >= sizeof(un.sun_path) - 8) {
-		errno = ENAMETOOLONG;
-		return(-1);
-	}
-
-	if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
-	    return(-1);
-
-	memset(&un, 0, sizeof(un));
-	un.sun_family = AF_UNIX;
-	sprintf(un.sun_path, "%s", sockpid);
-	len = offsetof(struct sockaddr_un, sun_path) + strlen(un.sun_path);
-	unlink(un.sun_path);
-	if (bind(fd, (struct sockaddr *)&un, len) < 0) {
-		rval = -2;
-		goto errout;
-	}
-	
-	if (chmod(un.sun_path, S_IRWXU) < 0) {
-		rval = -3;
-		do_unlink = 1;
-		goto errout;
-	}
-	
-	memset(&sun, 0, sizeof(sun));
-	sun.sun_family = AF_UNIX;
-	strncpy(sun.sun_path, sockpid, sizeof(sun.sun_path));
-	len = offsetof(struct sockaddr_un, sun_path) + strlen(sockpid);
-	DMSG("client connecting to %s\n", sockpid);
-	
-	if (connect(fd, (struct sockaddr *)&sun, len) < 0) {
-		DMSG("connect failure %s\n", sun.sun_path);
-		perror(NULL);
-		rval = -4;
-		do_unlink = 1;
-		goto errout;
-		
-	}
-	DMSG("connected...\n");
-	return(fd);
-errout:
-	err = errno;
-	close(fd);
-	if (do_unlink)
-		unlink(un.sun_path);
-	errno = err;
-	return(rval);
-}
-
-
 // WRS, with check for Linux EAGAIN
 ssize_t	readn(int fd, void *vptr, size_t n)
 {
