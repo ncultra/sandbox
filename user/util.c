eaf16d9acb95fae88ac3a71edb9e5d41760af088
Mike Day <ncmike@ncultra.org>
2017-03-15 16:31:09 -0400

    formatting clean-up

--- user/util.c
+++ user/util.c
@@ -8,30 +8,28 @@
 
 void *_zalloc (size_t size)
 {
-	void *buf = malloc (size);
-	if (!buf) {
-		fprintf (stderr,
-			 "Failed to allocate %Zu bytes of memory\n", size);
-		exit (1);
-	}
+    void *buf = malloc (size);
+    if (!buf) {
+	fprintf (stderr, "Failed to allocate %Zu bytes of memory\n", size);
+	exit (1);
+    }
 
-	memset (buf, 0, size);
+    memset (buf, 0, size);
 
-	return buf;
+    return buf;
 }
 
 
 void *_realloc (void *buf, size_t size)
 {
-	void *newbuf = realloc (buf, size);
-	if (!newbuf) {
-		fprintf (stderr,
-			 "Failed to reallocate %Zu bytes of memory\n",
-			 size);
-		exit (1);
-	}
+    void *newbuf = realloc (buf, size);
+    if (!newbuf) {
+	fprintf (stderr,
+		 "Failed to reallocate %Zu bytes of memory\n", size);
+	exit (1);
+    }
 
-	return newbuf;
+    return newbuf;
 }
 
 
@@ -43,64 +41,63 @@ void *_realloc (void *buf, size_t size)
 
 int _read_line (char *filename, char *buf, size_t bufsize)
 {
-	FILE *f = fopen (filename, "r");
-	if (!f) {
-		fprintf (stderr, "error: fopen(%s): %m\n", filename);
-		return -1;
-	}
+    FILE *f = fopen (filename, "r");
+    if (!f) {
+	fprintf (stderr, "error: fopen(%s): %m\n", filename);
+	return -1;
+    }
 
-	int failed = (fgets (buf, bufsize, f) == NULL);
-	fclose (f);
-	if (failed) {
-		fprintf (stderr, "error: fgets(%s): %m\n", filename);
-		return -1;
-	}
+    int failed = (fgets (buf, bufsize, f) == NULL);
+    fclose (f);
+    if (failed) {
+	fprintf (stderr, "error: fgets(%s): %m\n", filename);
+	return -1;
+    }
 
-	/* Strip off trailing \n */
-	size_t len = strlen (buf);
-	if (buf[len - 1] == '\n') {
-		buf[len - 1] = 0;
-		len--;
-	}
+    /* Strip off trailing \n */
+    size_t len = strlen (buf);
+    if (buf[len - 1] == '\n') {
+	buf[len - 1] = 0;
+	len--;
+    }
 
-	return len;
+    return len;
 }
 
 
 int get_xen_version (char *buf, size_t bufsize)
 {
-	char major[64], minor[64], extra[64];
+    char major[64], minor[64], extra[64];
 
-	if (_read_line (MAJORFILE, major, sizeof (major)) < 0)
-		return -1;
-	if (_read_line (MINORFILE, minor, sizeof (minor)) < 0)
-		return -1;
-	if (_read_line (EXTRAFILE, extra, sizeof (extra)) < 0)
-		return -1;
+    if (_read_line (MAJORFILE, major, sizeof (major)) < 0)
+	return -1;
+    if (_read_line (MINORFILE, minor, sizeof (minor)) < 0)
+	return -1;
+    if (_read_line (EXTRAFILE, extra, sizeof (extra)) < 0)
+	return -1;
 
-	return snprintf (buf, bufsize, "%s.%s%s", major, minor, extra);
+    return snprintf (buf, bufsize, "%s.%s%s", major, minor, extra);
 }
 
 
 int get_xen_compile_date (char *buf, size_t bufsize)
 {
-	return _read_line (COMPILEDATEFILE, buf, bufsize);
+    return _read_line (COMPILEDATEFILE, buf, bufsize);
 }
 
 int string2sha1 (const unsigned char *string, unsigned char *sha1)
 {
-	int i, ccode;
-	/* Make sure first 40 chars of string are composed of only hex digits */
-	for (i = 0; i < 40; i += 2) {
-		if ((ccode =
-		     sscanf ((const char *) string + i, "%02x",
-			     (int *) (&sha1[i / 2])))
-		    != 1) {
-			fprintf (stderr,
-				 "error: not a valid sha1 string: %s\n",
-				 string);
-			return -1;
-		}
+    int i, ccode;
+    /* Make sure first 40 chars of string are composed of only hex digits */
+    for (i = 0; i < 40; i += 2) {
+	if ((ccode =
+	     sscanf ((const char *) string + i, "%02x",
+		     (int *) (&sha1[i / 2])))
+	    != 1) {
+	    fprintf (stderr,
+		     "error: not a valid sha1 string: %s\n", string);
+	    return -1;
 	}
-	return 0;
+    }
+    return 0;
 }
